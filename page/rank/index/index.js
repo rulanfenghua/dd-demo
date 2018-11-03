9d3aa9dc6708f05baa1f1bd28c445cf1a445c003
abraham <rulanfenghua@163.com>
2018-11-03 18:10:01 +0800

    construct

--- page/rank/index/index.js
+++ page/rank/index/index.js
@@ -1,3 +1,56 @@
+let app = getApp()
+
 Page({
-  
+  data: {
+    items: []
+  },
+  onShow() {
+    dd.showLoading()
+    dd.httpRequest({
+      url: app.globalData.domain + '/rank',
+      method: 'POST',
+      dataType: 'json',
+      success: (res) => {
+        console.log('successRank----', res)
+        this.setData({
+          items: res.data.data
+        })
+      },
+      fail: (res) => {
+        console.log("httpRequestFailRank---", res)
+        dd.alert({
+          content: JSON.stringify(err)
+        })
+      },
+      complete: () => {
+        dd.hideLoading()
+      }
+    })
+  },
+  onReachBottom() {
+    dd.showLoading()
+    dd.httpRequest({
+      url: app.globalData.domain + '/rank',
+      method: 'POST',
+      dataType: 'json',
+      success: (res) => {
+        console.log('successRank----', res)
+        this.setData({
+          items: res.data.data
+        })
+      },
+      fail: (res) => {
+        console.log("httpRequestFailRank---", res)
+        dd.alert({
+          content: JSON.stringify(err)
+        })
+      },
+      complete: () => {
+        dd.hideLoading()
+      }
+    })
+  },
+  onItemClick({index}) {
+    console.log('list点击', index)
+  }
 })
