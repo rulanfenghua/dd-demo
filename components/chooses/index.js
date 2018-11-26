1e855c9f2064a1f696099d9e90a4003ff7a8a189
e <rulanfenghua@163.com>
2018-11-26 11:22:16 +0800

    chooses completed

--- components/chooses/index.js
+++ components/chooses/index.js
@@ -13,16 +13,15 @@ Component({
     search: '',
     to: [],
     changeTo: [],
-    midTo: []
+    users: []
   },
 
   didMount() {
     this.allUsers()
     this.setData({
-      to: [],
-      midTo: []
+      to: []
     })
-
+    
     dd.getSystemInfo({
       success: (res) => {
         var height = res.windowHeight
@@ -51,6 +50,11 @@ Component({
         success: (res) => {
           console.log('successUsers----', res)
           var users = res.data.data.list
+          if (users.length < this.data.users.length) {
+            this.setData({
+              changeTo: []
+            })
+          }
           users.forEach((item) => {
             if (this.data.to.some((toItem) => toItem.userId == item.userId)) {
               item.checked = true
@@ -58,8 +62,7 @@ Component({
           })
 
           this.setData({
-            users: users,
-            midTo: this.data.to
+            users: users
           })
         },
         fail: (res) => {
@@ -114,42 +117,42 @@ Component({
       this.props.onTo([])
     },
     onSubmit(e) {
-      console.log('changeTo', e.detail.value.to)
-      console.log('changeTo', this.data.changeTo)
-      console.log('midTo', this.data.midTo)
-      var to = this.data.midTo
-      this.data.changeTo.forEach((item) => {
-        if (to.some((toItem) => toItem.userId == item.userId)) {
-          var index = to.findIndex((findItem) => findItem.userId == item.userId)
-          to.splice(index, 1)
-        } else {
-          to.push(item)
-        }
-      })
-      console.log('to', to)
-      this.setData({
-        to: to
-      })
+      console.log('submitTo', e.detail.value.to)
 
-      this.props.onTo(to)
+      this.props.onTo(this.data.to)
 
       this.props.onShowFilter()
     },
     onChange(e) {
       console.log(e.detail.value)
-      // var changeTo = []
-      // e.detail.value.forEach((item) => {
-      //   if (this.data.to.some((toItem) => toItem.userId == item.userId)) {
-      //     changeTo.splice(changeTo.indexOf(item), 1)
-      //   } else {
-      //     changeTo.push(item)
-      //   }
-      // })
 
-      // console.log('changeTo', changeTo)
-
+      var change = {}
+      
+      if (e.detail.value.length > this.data.changeTo.length) {
+        change = e.detail.value[e.detail.value.length-1]
+      }
+      if (e.detail.value.length < this.data.changeTo.length) {
+        change = this.data.changeTo.find((item) => {
+          return !e.detail.value.some((findItem) => findItem.userId == item.userId)
+        })
+      }
       this.setData({
         changeTo: e.detail.value
+      })
+
+      console.log('change', change)
+
+      var to = this.data.to
+      
+      if (to.some((item) => item.userId == change.userId)) {
+        var index = to.findIndex((findItem) => findItem.userId == change.userId)
+        to.splice(index, 1)
+      } else {
+        to.push(change)
+      }
+      console.log('to', to)
+      this.setData({
+        to: to
       })
     }
   }
