16586001532e8c4bc8f4a112e54d66599dbafbeb
e <rulanfenghua@163.com>
2018-12-20 10:30:33 +0800

    fix

--- app.js
+++ app.js
@@ -14,10 +14,6 @@ App({
   globalData: {
     corpId: '',
     level: '',
-<<<<<<< HEAD
     domain: 'http://jifen.meisijia.cn:8081'
-=======
-    domain: 'http://192.168.0.3:8081'
->>>>>>> master
   }
 });
